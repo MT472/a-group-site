3d0ecbe2a898b526ba57b0138346f45878098e81
MT472 <mohammad.47.taji@gmail.com>
2024-02-23 21:40:53 +0330

    all of work

--- mysite/myapp/urls.py
+++ mysite/myapp/urls.py
@@ -1,10 +1,19 @@
 from django.contrib import admin
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
+    path('signup/',views.register,name='signup'),
     path('',views.index,name='index'),
-    path('signup/',views.signup,name='signup'),
+    path('login/',views.user_login,name='login'),
+    path('logout/',LogoutView.as_view(),name='logout'),
     path('aboutus/',views.aboutus,name='aboutus'),
-    path('login/',views.login,name='login'),
+    path('html/',views.html,name='html'),
+    path('css/',views.css,name='css'),
+    path('js/',views.js,name='js'),
+    path('django/',views.dj,name='django'),
+    path('python/',views.python,name='python'),
+    path('invalid/',views.Invalid,name='invalid')
 ]
--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -1,23 +1,62 @@
 from django.shortcuts import render,redirect
-from .models import account
-from .forms import accountForm
+from django.contrib.auth import authenticate,login
+from django.contrib import messages
+from django.views.generic import CreateView
+from django.contrib.auth.forms import AuthenticationForm,UserCreationForm
+from django.contrib.auth.decorators import login_required
 
-
-def signup(request):
+def register(request):
     if request.method=='POST':
-        user_account=accountForm(request.POST,request.FILES)
-        if user_account.is_valid():
-            user_account.save()
+        form=UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username=form.cleaned_data.get('username')
+            raw_password=form.cleaned_data('password1')
+            user=authenticate(username=username,password=raw_password)
+            login(request,user)
             return redirect('index')
-    user_account=accountForm()
-    return render(request,'myapp/form2.html',{'user_account':user_account})
+    else:
+        form=UserCreationForm()
+    return render(request,'myapp/form2.html',{'form':form})
 
+def user_login(request):
+    form=AuthenticationForm(data=request.POST)
+    if request.method=='POST':
+        username=form.cleaned_data.get('username')
+        password=form.cleaned_data.get('password')
+        user=authenticate(username=username,password=password)
+        if user is not None:
+            login(request,login)
+            return redirect('index')
+    else:
+        form=AuthenticationForm()
+    return render(request,'myapp/form1.html',{'form':form})
+
+@login_required
+def account_detail(request):
+    return render(request,'myapp/account.html')
 
 def aboutus(request):
     return render(request,'myapp/aboutUs.html')
 
-def login(request):
-    return render(request,'myapp/form1.html')
+def Invalid(request):
+    return render(request,'myapp/invalid.html')
 
+    
 def index(request):
     return render(request,'myapp/index.html')
+
+def html(request):
+    return render(request,'myapp/page1.html')
+
+def css(request):
+    return render(request,'myapp/page2.html')
+
+def js(request):
+    return render(request,'myapp/page3.html')
+
+def dj(request):
+    return render(request,'myapp/page4.html')
+
+def python(request):
+    return render(request,'myapp/page5.html')
